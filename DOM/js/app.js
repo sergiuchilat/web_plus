ec6da519924185ee44b1c1a075f3b8c7080a97e0
SergiuChilat <chilatsergiu@gmail.com>
2020-02-11 09:16:36 +0200

    DOM

---
+++ DOM/js/app.js
@@ -0,0 +1,47 @@
+class Students{
+  constructor (students) {
+    this.students = students
+    
+  }
+  
+  setStudents(students){
+    this.students = students
+  }
+  
+  prepareForView(){
+    let result = [];
+    for(let i = 0; i < this.students.length; i++){
+      result.push(`${this.students[i].name}`)
+    }
+    const containerContent = result.join('<br>');
+    return `Students list: <br/>
+            ${containerContent}
+            <hr/>
+            `;
+  }
+  
+  
+}
+
+let students = [
+  {
+    name: "Ion",
+    age: 19,
+    mark: 8.5
+  },
+  {
+    name: "Petru",
+    age: 20,
+    mark: 8.7
+  },
+  {
+    name: "Mihai",
+    age: 22,
+    mark: 10.3
+  }
+];
+
+
+let grupa = new Students(students);
+grupa.setStudents(students);
+document.getElementById('container').innerHTML = grupa.prepareForView();
